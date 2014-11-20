31a20455928ccca22e52ef3a55a25d936d8d1649
mlockley@elon.edu <melissalockley@Melissa-Lockleys-MacBook-Pro.local>
2014-11-19 20:51:57 -0500

    initial files

---
+++ app/models/owner.rb
@@ -0,0 +1,25 @@
+class Owner
+
+	def name
+		name = "Melissa"
+	end
+
+	def birthdate
+		birthday = Date.new(1991, 6, 5)
+	end
+
+	def countdown
+		today = Date.today
+
+		birthday = Date.new(today.year, birthdate.month, birthdate.day)
+
+		if birthday > today 
+			countdown = (birthday - today).to_i
+		elsif birthday < today
+			countdown = (birthday.next_year - today).to_i
+		else
+			return "Happy Birthday!"
+		end
+	end
+
+end
